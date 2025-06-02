823e25e4b058635c7488b66bc1902fab62e72116
Joe Corrales <corralesjoe07@gmail.com>
2025-06-02 10:11:22 -0600

    Pagina de inicio y menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Link } from 'react-router';
+import unitecLogo from './assets/unitec.jpeg' // asegúrate de tener este archivo
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="cover-container">
+      <img src={unitecLogo} alt="Logo Unitec" className="unitec-logo" />
+      <h1 className="title">Valeria Alfaro</h1>
+      <h1 className="title">Harold Diaz</h1>
+      <h1 className="title">Joe Corrales</h1>
+      <Link to="/menu" className="menu-button">
+        Menú
+      </Link>
+    </div>
   )
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+
+import { BrowserRouter, Routes, Route } from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Menu from './pages/Menu.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path="/menu" element={<Menu />} />
+    </Routes>
+  </BrowserRouter>
 )
