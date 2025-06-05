a59d9cfd16035f7561a5ecdaa942a7140775d06d
valeAlfaro5 <valeriasalfaro@gmail.com>
2025-06-05 09:28:40 -0600

    Revert "Merge branch 'valeria'"
    This reverts commit 24c7ffa43e2237249e24dda58558e73c061702e9, reversing
    changes made to 03cb95f2887d8a33ef193a5da7d0ea23fe9e8831.
    
    revert de branch#

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 import App from './App.jsx'
 import Menu from './pages/Menu.jsx'
 import Particion from './pages/Particion.jsx';
-import ColoracionGrafosComunidad from './pages/ColoracionGrafosComunidad.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -13,7 +12,6 @@ createRoot(document.getElementById('root')).render(
       <Route path='/' element={<App />} />
       <Route path="/menu" element={<Menu />} />
       <Route path="/particion" element={<Particion />} />
-      <Route path="/coloracion-grafos-comunidad" element={<ColoracionGrafosComunidad/>}/>
     </Routes>
   </BrowserRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@backend': path.resolve(__dirname, './backend')
-    }
-  }
+  plugins: [react(), tailwindcss(),],
 })
