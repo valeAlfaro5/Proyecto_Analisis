14b277eb7a8204c0c3f2ec5a484a664b79df19aa
valeAlfaro5 <valeriasalfaro@gmail.com>
2025-06-15 10:44:40 -0600

    no se importaron los menus de valeria

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,7 @@ import Menu from './pages/Menu.jsx'
 import Particion from './pages/Particion.jsx';
 import Algoritmo_Propio from './pages/Problem_Partition/Algoritmo_Propio.jsx'
 import Algoritmo_Comunidad from './pages/Problem_Partition/Algoritmo_Comunidad.jsx'
-
-//import ColoracionGrafosComunidad from './pages/coloracion/ColoracionComunidad.jsx';
+import ColoracionGrafosComunidad from './pages/coloracion/ColoracionComunidad.jsx';
 import ColoracionMenu from './pages/coloracion/ColoracionMenu.jsx'
 import ColoracionPropio from './pages/coloracion/ColoracionPropio.jsx'
 import HamiltonianMenu from './pages/hamiltonian cycles/HamiltonianMenu.jsx'
@@ -22,6 +21,8 @@ createRoot(document.getElementById('root')).render(
       <Route path="/menu" element={<Menu />} />
       <Route path="/particion" element={<Particion />} />
       <Route path="/coloracion-grafos-comunidad" element={<ColoracionGrafosComunidad/>}/>
+      <Route path="/coloracion-grafos-propio" element={<ColoracionPropio/>}/>
+      <Route path="/coloracion-grafos-menu" element={<ColoracionMenu/>}/>
       <Route path="/hamiltonian-menu" element={<HamiltonianMenu />}/>
       <Route path='/hamiltonian-community' element={<HamiltonianCommunity />}/>
       <Route path="/Algoritmo_Propio" element={<Algoritmo_Propio />} />
